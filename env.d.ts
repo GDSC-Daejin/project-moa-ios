b218535afac82f9dda2501765b97a3bec8bfee07
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-07-23 21:34:38 +0900

    firebase 초기세팅

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,8 @@
 declare module "@env" {
   export const KAKAO_API_KEY: string;
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const PROJECT_ID: string;
+  export const STORAGE_BUCKET: string;
+  export const APP_ID: string;
 }
---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { API_KEY, APP_ID, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET } from "@env";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  appId: APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
